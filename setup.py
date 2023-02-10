fb83dedbf22e7ea80aea5bb216ae5f9070965bea
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2023-02-10 17:49:40 -0500

    fix(build): change the ownership from ladybug-tools to pollination

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 # normal setuptool inputs
 setuptools.setup(
     name='pollination-appendix-g-performance',                                     # will be used for package name unless it is overwritten using __queenbee__ info.
-    author='ladybug-tools',                                                 # the owner account for this package - required if pushed to Pollination
+    author='pollination',                                                 # the owner account for this package - required if pushed to Pollination
     author_email='info@ladybug.tools',
     packages=setuptools.find_namespace_packages(                            # required - that's how pollination find the package
         include=['pollination.*'], exclude=['tests', '.github']
